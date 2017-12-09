200db69ef3723939d612821fc9331a205e646cc0
Michael A Tomcal <michael@twoeyetech.com>
2017-12-09 12:28:52 -0700

    Added tests for SearchForm

--- src/forms/__tests__/SearchForm.test.js
+++ src/forms/__tests__/SearchForm.test.js
@@ -31,6 +31,53 @@ describe('SearchForm', () => {
         wrapper.find('Field').find('input[name="gene"][value="stuff"]'),
       ).toHaveLength(1);
     });
-    describe('form modification', () => {});
+    describe('form modification', () => {
+      const testFieldHelper = (name, value, expectedResult) => {
+        const wrapper = mount(<SearchForm {...props} />);
+        wrapper.mount();
+        wrapper.find(`[name="${name}"]`).simulate('change', {
+          target: {
+            id: name,
+            value,
+          },
+        });
+        return sleep(1)
+          .then(() => {
+            // Don't simulate if you use form based submit but use DOM click instead
+            // otherwise the form context is lost
+            wrapper
+              .find('button[type="submit"]')
+              .get(0)
+              .click();
+            return sleep(1);
+          })
+          .then(() => {
+            expect(props.onSubmit).toHaveBeenCalled();
+            // Get first call and first argument to compare
+            expect(props.onSubmit.mock.calls[0][0]).toEqual(expectedResult);
+          });
+      };
+      it('successfully submits keyword search', () => {
+        return testFieldHelper('keyword', 'MySearchString', {
+          gene: 'stuff',
+          keyword: 'MySearchString',
+          pog: '',
+        });
+      });
+      it('successfully submits gene search', () => {
+        return testFieldHelper('gene', 'MySearchString', {
+          gene: 'MySearchString',
+          keyword: '',
+          pog: '',
+        });
+      });
+      it('successfully submits pog search', () => {
+        return testFieldHelper('pog', 'MySearchString', {
+          gene: 'stuff',
+          keyword: '',
+          pog: 'MySearchString',
+        });
+      });
+    });
   });
 });
